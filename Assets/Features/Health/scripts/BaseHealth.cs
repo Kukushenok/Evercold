55d6fabb6b4641cf66394575cab73794e3c1382a
svvoooyyyy <79079738+svvoooyyyy@users.noreply.github.com>
2024-11-23 17:00:42 +0300

    added zenject dependency

--- Assets/Features/Health/scripts/BaseHealth.cs
+++ Assets/Features/Health/scripts/BaseHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseHealth : MonoBehaviour
+public abstract class BaseHealth : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
