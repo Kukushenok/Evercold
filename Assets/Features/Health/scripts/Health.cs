cd846a4960d7db999ad0f012e4a37c08e7c02f4a
svvoooyyyy <79079738+svvoooyyyy@users.noreply.github.com>
2024-11-24 17:32:34 +0300

    namespace changed

--- Assets/Features/Health/scripts/Health.cs
+++ Assets/Features/Health/scripts/Health.cs
@@ -4,32 +4,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Health : MonoBehaviour
+namespace Feature.Health
 {
-    [SerializeField] public float MaxHealth { private set; get; }
-    [SerializeField] public float CurrentHealth { private set; get; }
-
-    public UnityEvent OnTakeDamage;
-    public UnityEvent OnHeal;
-    public UnityEvent OnDie;
-
-    public void GetDamage(float dmg)
+    public class Health : MonoBehaviour
     {
-        if (CurrentHealth == 0) { return; }
-        if (dmg < 0) Debug.LogError("Can't take damage, damage must be > 0");
+        [SerializeField] public float MaxHealth { private set; get; }
+        [SerializeField] public float CurrentHealth { private set; get; }
 
-        OnTakeDamage?.Invoke();
-        CurrentHealth -= dmg;
-        if (CurrentHealth <= 0) { CurrentHealth = 0f; OnDie?.Invoke(); }
-    }
+        public UnityEvent OnTakeDamage;
+        public UnityEvent OnHeal;
+        public UnityEvent OnDie;
 
-    public void Heal(float value)
-    {
-        if (CurrentHealth == MaxHealth) { return; }
-        if (value < 0) Debug.LogError("Can't heal, value must be > 0");
+        public void GetDamage(float dmg)
+        {
+            if (CurrentHealth == 0) { return; }
+            if (dmg < 0) Debug.LogError("Can't take damage, damage must be > 0");
 
-        OnHeal?.Invoke();
-        CurrentHealth += value;
-        if (CurrentHealth >= MaxHealth) CurrentHealth = MaxHealth;
+            OnTakeDamage?.Invoke();
+            CurrentHealth -= dmg;
+            if (CurrentHealth <= 0) { CurrentHealth = 0f; OnDie?.Invoke(); }
+        }
+
+        public void Heal(float value)
+        {
+            if (CurrentHealth == MaxHealth) { return; }
+            if (value < 0) Debug.LogError("Can't heal, value must be > 0");
+
+            OnHeal?.Invoke();
+            CurrentHealth += value;
+            if (CurrentHealth >= MaxHealth) CurrentHealth = MaxHealth;
+        }
     }
 }
