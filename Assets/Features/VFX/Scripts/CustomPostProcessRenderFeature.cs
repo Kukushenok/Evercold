0902877b6229b1590d3fc5057392a1794bb1bb9e
IlyaP <mrcoolmoder@gmail.com>
2025-01-21 21:54:28 +0300

    I love this effect. This is stupid but I love it. When you look into the bright objects your eyes burn a little

--- Assets/Features/VFX/Scripts/CustomPostProcessRenderFeature.cs
+++ Assets/Features/VFX/Scripts/CustomPostProcessRenderFeature.cs
@@ -12,9 +12,11 @@ namespace Features.VFX
 
     public class CustomPostProcessRenderFeature : ScriptableRendererFeature, IDisposable
     {
-        [SerializeField] private Shader _fShader;
-        private CustomPostProcessRenderPass _pass; 
-        private Material _material;
+        [SerializeField] private Shader _blindUpdateShader;
+        [SerializeField] private Shader _blindApplyShader;
+        private SunBlindnessPostProcessRenderPass _pass; 
+        private Material _blindUpdateMaterial;
+        private Material _blindApplyMaterial;
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
             if (renderingData.cameraData.cameraType == CameraType.Game)
@@ -23,15 +25,16 @@ namespace Features.VFX
 
         public override void Create()
         {
-            _material = CoreUtils.CreateEngineMaterial(_fShader);
-            _pass = new CustomPostProcessRenderPass(_material);
+            _blindUpdateMaterial = CoreUtils.CreateEngineMaterial(_blindUpdateShader);
+            _blindApplyMaterial = CoreUtils.CreateEngineMaterial(_blindApplyShader);
+            _pass = new SunBlindnessPostProcessRenderPass(_blindUpdateMaterial, _blindApplyMaterial);
 
         }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                CoreUtils.Destroy(_material);
+                CoreUtils.Destroy(_blindUpdateMaterial);
             }
         }
         public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
@@ -40,22 +43,23 @@ namespace Features.VFX
         }
     }
     [System.Serializable]
-    public class CustomPostProcessRenderPass : ScriptableRenderPass
+    public class SunBlindnessPostProcessRenderPass : ScriptableRenderPass
     {
-        public readonly Material Material;
+        public readonly Material BlindApplyMaterial;
+        public readonly Material BlindUpdateMaterial;
         //private RenderTextureDescriptor _descriptor;
         private RTHandle _camColorHandle;
-        private RTHandle _camDerpthHandle;
-        private RenderTextureDescriptor _lagBackDescriptor;
-        private RTHandle _lagBackHandle = null;
-        private RTHandle _camResultHandle = null;
-        public CustomPostProcessRenderPass(Material material)
+        private RenderTextureDescriptor _blindDescriptor;
+        private RTHandle _blindDescriptorStep = null;
+        private RTHandle _lightBlinderTexture = null;
+        public SunBlindnessPostProcessRenderPass(Material blindUpdateMaterial, Material blindApplyMaterial)
         {
-            _lagBackDescriptor = new RenderTextureDescriptor(Screen.width,
-        Screen.height, RenderTextureFormat.Default, 0);
+            _blindDescriptor = new RenderTextureDescriptor(Screen.width,
+        Screen.height, RenderTextureFormat.BGR101010_XR, 0);
             renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
-            Material = material;
-            _lagBackHandle = null;
+            BlindApplyMaterial = blindApplyMaterial;
+            BlindUpdateMaterial = blindUpdateMaterial;
+            _lightBlinderTexture = null;
             ConfigureInput(ScriptableRenderPassInput.Color | ScriptableRenderPassInput.Depth);
         }
 
@@ -63,21 +67,22 @@ namespace Features.VFX
         {
             // get the source target from rendering data every frame
             _camColorHandle = renderingData.cameraData.renderer.cameraColorTargetHandle;
-            _camDerpthHandle = renderingData.cameraData.renderer.cameraDepthTargetHandle;
         }
         public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
         {
             //Set the red tint texture size to be the same as the camera target size.
-            _lagBackDescriptor.width = cameraTextureDescriptor.width;
-            _lagBackDescriptor.height = cameraTextureDescriptor.height;
+            _blindDescriptor.width = cameraTextureDescriptor.width;
+            _blindDescriptor.height = cameraTextureDescriptor.height;
+
             //Check if the descriptor has changed, and reallocate the RTHandle if necessary.
         }
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             if (renderingData.cameraData.isPreviewCamera) return;
-            if (_camColorHandle == null || _camDerpthHandle == null) return;
-            RenderingUtils.ReAllocateIfNeeded(ref _lagBackHandle, _lagBackDescriptor, name: "_LagBackTexture");
-            RenderingUtils.ReAllocateIfNeeded(ref _camResultHandle, _lagBackDescriptor, name: "_CamResultTexture");
+            if (_camColorHandle == null) return;
+            RenderingUtils.ReAllocateIfNeeded(ref _lightBlinderTexture, _blindDescriptor, name: "_BlindTexture");
+            RenderingUtils.ReAllocateIfNeeded(ref _blindDescriptorStep, _blindDescriptor, name: "_BlindTextureStep");
+            //RenderingUtils.ReAllocateIfNeeded(ref _lightBlinderTexture, _blindDescriptor, name: "_BlindTexture");
             VolumeStack st = VolumeManager.instance.stack;
             MeatVolumeEffect eff = st.GetComponent<MeatVolumeEffect>();
             CommandBuffer cmd = CommandBufferPool.Get();
@@ -93,13 +98,16 @@ namespace Features.VFX
         }
         private void DoRendering(CommandBuffer cmd)
         {
-            Debug.Log(_lagBackHandle);
-            Material.SetTexture("_LagBackTexture", _lagBackHandle);
+            //Debug.Log(_lightBlinderTexture);
+            BlindUpdateMaterial.SetTexture("_BlindTexture", _lightBlinderTexture);
+            BlindUpdateMaterial.SetTexture("_CameraTexture", _camColorHandle);
+            BlindApplyMaterial.SetTexture("_BlindTexture", _lightBlinderTexture);
+
             //cmd.SetGlobalTexture("_LagBackTexture", _lagBackHandle);
             //cmd.SetGlobalTexture("_CameraColorTexture", _camColorHandle);
-            Blit(cmd, _camColorHandle, _camResultHandle);
-            Blit(cmd, _camColorHandle, _camColorHandle, Material);
-            Blit(cmd, _camResultHandle, _lagBackHandle);
+            Blit(cmd, _lightBlinderTexture, _blindDescriptorStep, BlindUpdateMaterial);
+            Blit(cmd, _camColorHandle, _camColorHandle, BlindApplyMaterial);
+            Blit(cmd, _blindDescriptorStep, _lightBlinderTexture);
         }
     }
 }
