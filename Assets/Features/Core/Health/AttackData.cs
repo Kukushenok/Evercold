7d0ba4880f68614bec9ad637c9ad33845bd89717
svvoooyyyy <79079738+svvoooyyyy@users.noreply.github.com>
2025-03-29 12:44:14 +0300

    small fix
    added constructor to AttackData
    moved some scripts to other folders

---
+++ Assets/Features/Core/Health/AttackData.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackData
+{
+    public float Damage {private set; get;}
+    GameObject? AssociatedOwner;
+    Vector3? AssociatedKnockback;
+
+    
+
+    // public AttackData(float damage) => Damage = damage;
+    public AttackData(float damage, GameObject? associatedOwner, Vector3? associatedKnockback){
+        Damage = damage;
+        AssociatedOwner = associatedOwner;
+        AssociatedKnockback = associatedKnockback;
+    }
+
+    public AttackData(float damage, GameObject? associatedOwner){
+        Damage = damage;
+        AssociatedOwner = associatedOwner;
+        AssociatedKnockback = null;
+    }
+    public AttackData(float damage, Vector3? associatedKnockback){
+        Damage = damage;
+        AssociatedOwner = null;
+        AssociatedKnockback = associatedKnockback;
+    }
+    public AttackData(float damage){
+        Damage = damage;
+        AssociatedOwner = null;
+        AssociatedKnockback = null;
+    }
+}
